c2701802fbfd94310af80e51f9cf8127e45b8506
Michael Gutierrez <michaelgutierrezj@gmail.com>
2022-04-12 19:02:45 -0500

    get get post put endpoints implemented

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -11,8 +11,55 @@ router.get('/', (req, res) => {
             res.status(200).json(posts)
         })
         .catch(error => {
-            res.status(500).json({ message: 'Error retrieving posts'})
+            res.status(500).json({ message: "The posts information could not be retrieved" })
         })
 });
+
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist" })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: "The post information could not be retrieved" })
+        })
+});
+
+router.post('/', (req, res) => {
+    if(!req.body.title || !req.body.contents) {
+        res.status(400).json({ message: "Please provide title and contents for the post" })
+        return;
+    }
+    Posts.insert(req.body)
+        .then(post => {
+            res.status(201).json({ ...req.body, id: post.id });
+        })
+        .catch(error => {
+            res.status(500).json({ message: "There was an error while saving the post to the database" })
+        })
+})
+
+router.put('/:id', (req, res) => {
+    const changes = req.body
+    if(!changes.title || !changes.contents) {
+        res.status(400).json({ message: "Please provide title and contents for the post" })
+        return;
+    }
+    Posts.update(req.params.id, changes)
+        .then(post => {
+            if (post) {
+                res.status(200).json({ ...changes, id: Number(req.params.id) });
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist" })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: "The post information could not be modified" })
+        })
+})
 
 module.exports = router;
